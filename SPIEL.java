590855c469c6afee3924adf247417d68a15e2735
h̸̩̰̜͚͖̠̖͔̆̂͝ͅa̷̡̡̨̛̩͔̹̬͖̼̎͜h̴̺̣̗̟̖̍̊͠a̵̘̳̭̺͈̥̳̤̽͊̍̽͂̀̉̈́͑̉̔̕͠h̴̨̡̛̙͔̗̯̱̲͉̥̼̘̫̋̓̈́̀̉̏̽͑́͌̈́͘͘͜͝a̴̛̺͚̺̘̺̜̗͓̪̮̎ <jakob.bruederlein@gmail.com>
2022-06-21 10:22:34 +0200

    klasse spiel aufgeräumt

--- SPIEL.java
+++ SPIEL.java
@@ -1,33 +1,8 @@
 
-/**
- * Beschreiben Sie hier die Klasse SPIEL.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class SPIEL
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse SPIEL
-     */
-    public SPIEL()
+  public SPIEL()
     {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+        
     }
 }

a85fe3f7df3837b620fee77d21c4cf5065ba0107
h̸̩̰̜͚͖̠̖͔̆̂͝ͅa̷̡̡̨̛̩͔̹̬͖̼̎͜h̴̺̣̗̟̖̍̊͠a̵̘̳̭̺͈̥̳̤̽͊̍̽͂̀̉̈́͑̉̔̕͠h̴̨̡̛̙͔̗̯̱̲͉̥̼̘̫̋̓̈́̀̉̏̽͑́͌̈́͘͘͜͝a̴̛̺͚̺̘̺̜̗͓̪̮̎ <jakob.bruederlein@gmail.com>
2022-06-21 10:39:12 +0200

    klassen aufgeräumt

--- TICKER.java
+++ TICKER.java
@@ -1,33 +1,12 @@
 
-/**
- * Beschreiben Sie hier die Klasse TICKER.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class TICKER
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse TICKER
-     */
+    
     public TICKER()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    
+    
 }

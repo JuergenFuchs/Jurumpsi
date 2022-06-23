b7ff38d317de4fdfbbddc904b500a215d1a901f2
heckerman24 <heckerman24@gmail.com>
2022-06-23 15:26:24 +0200

    9

--- MAP.java
+++ MAP.java
@@ -1,7 +1,14 @@
 
 public class MAP
 {
-   
+    //Knotenanzahl
+    private int anzahlKnoten;
+    
+    // Feld der Knoten des Graphen
+    private KNOTEN[] knoten;   
+
+    // 2-dim Feld der Adjazenzmatrix
+    private int[][] matrix;
     public MAP()
     {
        
